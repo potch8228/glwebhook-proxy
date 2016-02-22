68893cea6dfce756864c61e3e7d2e9581655c4da
Tatsuya Kobayashi <pikopiko28@gmail.com>
2016-02-22 20:37:19 +0900

    Fix flag parse; Add GitHub/GitHub Enterprise support

--- README.md
+++ README.md
@@ -18,6 +18,11 @@ Protocol part of "://" need to be replaced with "-" (single minus sign); for oth
 e.g.
 https://thisproxy.example.com/https-targetciserver.example.com/job/test-ci-job
 
+Command Flags
+----
+-port (default: 8080): proxy listen port
+-github=true (default: false): proxy for GitHub/GitHub Enterprise; Can not work side-by-side with GitLab CE mode
+
 Compile Environment
 ----
 go 1.5.3 darwin/amd64
--- glproxy.go
+++ glproxy.go
@@ -25,16 +25,21 @@ func parseURL(raw string) (t_url string) {
 	return
 }
 
-func parseArgs() (p int) {
-	p = *(flag.Int("port", 8080, "Listening port number: 1024 <= port <= 65535"))
+func parseArgs() (p int, gh bool) {
+	flag.IntVar(&p, "port", 8080, "Listening port number: 1024 <= port <= 65535")
 	if p < 1024 || p > 65535 {
 		log.Fatalln("Invalid Argument!")
 	}
+
+	flag.BoolVar(&gh, "github", false, "For GitHub/GitHub Enterprise WebHook format: default: false = for GitLab CE")
+	flag.Parse()
 
 	return
 }
 
 var u_proto_regex = regexp.MustCompile(`(http(|s))-`)
+
+var github = false
 
 func proxyGLWebHook(w http.ResponseWriter, r *http.Request) {
 	t_url := parseURL(r.URL.Path)
@@ -43,7 +48,18 @@ func proxyGLWebHook(w http.ResponseWriter, r *http.Request) {
 		msg(w, "Failed to create request", http.StatusInternalServerError)
 		return
 	}
-	procr.Header.Add("X-Gitlab-Event", r.Header.Get("X-Gitlab-Event"))
+
+	if github {
+		procr.Header.Add("X-Github-Delivery", r.Header.Get("X-Github-Delivery"))
+		procr.Header.Add("X-Github-Event", r.Header.Get("X-Github-Event"))
+
+		if sig := r.Header.Get("X-Hub-Signature"); len(sig) != 0 {
+			procr.Header.Add("X-Hub-Signature", sig)
+		}
+	} else {
+		procr.Header.Add("X-Gitlab-Event", r.Header.Get("X-Gitlab-Event"))
+	}
+
 	client := &http.Client{
 		Timeout: time.Second * 10,
 	}
@@ -57,9 +73,15 @@ func proxyGLWebHook(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	p := parseArgs()
+	p, gh := parseArgs()
+	github = gh
 	http.HandleFunc("/", proxyGLWebHook)
 	lurl := fmt.Sprintf(":%d", p)
 	log.Println("Trying to Listening on: " + lurl)
+	if github {
+		log.Println("Webhooks Proxy for GitHub mode")
+	} else {
+		log.Println("Webhooks Proxy for GitLab CE mode")
+	}
 	log.Fatalln(http.ListenAndServe(lurl, nil))
 }
